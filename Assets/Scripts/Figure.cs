2b7fd65da75e752919e26b2b175e49d7ca8adbfa
DariaTiessova <bermuds@inbox.ru>
2021-09-27 13:18:49 +0600

    Стена с табличками
    4) На сцене есть стена, на которой есть 3 таблички с кубом, капсулой и шаром. Необходимо применить предмет к соответствующей табличке (по кнопке F). После применения предмет исчезает из рук и можно брать другой.

--- Assets/Scripts/Figure.cs
+++ Assets/Scripts/Figure.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Figure : MonoBehaviour
 {
     [HideInInspector]public bool taken;
+    [HideInInspector] public bool findPlace;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,5 +20,18 @@ public class Figure : MonoBehaviour
             transform.Rotate(Vector3.up, 100f * Time.deltaTime);
         }
        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag(gameObject.transform.tag + "Place"))
+        {
+            findPlace = true;
+            Debug.Log(gameObject.tag + " is on place");
+        }
+        else
+        {
+            findPlace = false;
+        }
     }
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public GameObject takePlace;
     bool takenFigure;
+    GameObject figureInHand;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +22,10 @@ public class PlayerController : MonoBehaviour
             {
                 CheckFigureAround(gameObject.transform.position, 5f);
             }
-            
+            else
+            {
+                PlacedFigure();
+            }
         }
     }
 
@@ -32,14 +36,26 @@ public class PlayerController : MonoBehaviour
         {
             if (hitCollider.CompareTag("Cube") || hitCollider.CompareTag("Sphere") || hitCollider.CompareTag("Capsul"))
             {
-                
-                hitCollider.GetComponent<Figure>().taken = true;
-              
-                hitCollider.transform.position = takePlace.transform.position;
-                hitCollider.transform.parent = takePlace.transform;
-                takenFigure = true;
+                if (!takenFigure)
+                {
+                    takenFigure = true;
+                    hitCollider.GetComponent<Figure>().taken = true;
+
+                    hitCollider.transform.position = takePlace.transform.position;
+                    hitCollider.transform.parent = takePlace.transform;
+                    figureInHand = hitCollider.gameObject;
+                }
             }
            
+        }
+    }
+
+    void PlacedFigure()
+    {
+       if(figureInHand.GetComponent<Figure>().findPlace)
+        {
+            figureInHand.SetActive(false);
+            takenFigure = false;
         }
     }
 }
